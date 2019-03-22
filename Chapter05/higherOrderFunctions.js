3715537d6bcfcb40b0ac3f0430312401938b756d
ago <ago@sitecore.net>
2019-03-22 18:33:05 +0200

    higher order functions example

---
+++ Chapter05/higherOrderFunctions.js
@@ -0,0 +1,19 @@
+function greaterThan(n) {
+    return m => m > n;
+}
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
+
+function unless(test, then) {
+    if (!test) then();
+}
+
+function repeat(times, body) {
+    for (var i = 0; i < times; i++) body(i);
+}
+
+repeat(3, function(n){
+    unless(n % 2, function() {
+        console.log(n, "is even");
+    });
+});
