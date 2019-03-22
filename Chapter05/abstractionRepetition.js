f6dd7b88cf79bdde5bbffc68c9414489d6f3ddd3
root <root@LT-AGO1-T-UA.dk.sitecore.net>
2019-03-22 17:37:00 +0200

    add latest changes

---
+++ Chapter05/abstractionRepetition.js
@@ -0,0 +1,20 @@
+// repeatLog(10);
+// repeat(10, console.log);
+
+let labels = [];
+repeat(5, i => {
+    labels.push(`Unit ${i + 1}`);
+});
+console.log(labels);
+
+function repeatLog(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+    }
+}
+
+function repeat(n, action) {
+    for (let i = 0; i < n; i++) {
+        action(i);
+    }
+}
