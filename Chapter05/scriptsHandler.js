21861c0946a2b924aa35e2788e890840468ca4d8
ago <ago@sitecore.net>
2019-03-25 18:15:51 +0200

    Add reduce sample

--- Chapter05/scriptsHandler.js
+++ Chapter05/scriptsHandler.js
@@ -1,6 +1,8 @@
 let SCRIPTS = require('./scripts.js');
 
 console.log(SCRIPTS.length);
+
+
 // console.log(filter(SCRIPTS, script => script.living));
 console.log(SCRIPTS.filter(script => script.living));
 
@@ -8,6 +10,16 @@ let rtlScripts = SCRIPTS.filter(s => s.direction == "rtl");
 // let rtlScriptsNames = map(rtlScripts, s=> s.name);
 // console.log(rtlScriptsNames);
 console.log(rtlScripts.map(s => s.name));
+
+// console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 0));
+console.log([1, 2, 3, 4].reduce((a, b) => a + b));
+
+console.log(SCRIPTS.reduce((a, b) => {
+    return characterCount(a) < characterCount(b) ? b : a;
+}));
+
+console.log(Math.round(average(SCRIPTS.filter(s => s.living).map(s => s.year))));
+console.log(Math.round(average(SCRIPTS.filter(s => !s.living).map(s => s.year))));
 
 function filter(array, test) {
     let passed = [];
@@ -25,4 +37,22 @@ function map(array, transform) {
         mapped.push(transform(element));
     }
     return mapped;
+}
+
+function reduce(array, combine, start) {
+    let current = start;
+    for (let element of array) {
+        current = combine(current, element);
+    }
+    return current;
+}
+
+function characterCount(script) {
+    return script.ranges.reduce((count, [from, to]) => {
+        return count + (to - from);
+    }, 0);
+}
+
+function average(array) {
+    return array.reduce((a, b) => a + b) / array.length;
 }
