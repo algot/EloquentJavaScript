c74a7ec9dec54f3361575844e663449f17e376bc
ago <ago@sitecore.net>
2019-03-25 13:20:40 +0200

    Scripts filter implemented

---
+++ Chapter05/scriptsHandler.js
@@ -0,0 +1,14 @@
+let SCRIPTS = require('./scripts.js');
+
+console.log(SCRIPTS.length);
+console.log(filter(SCRIPTS, script => script.living));
+
+function filter(array, test) {
+    let passed = [];
+    for (let element of array) {
+        if (test(element)) {
+            passed.push(element);
+        }
+    }
+    return passed;
+}
