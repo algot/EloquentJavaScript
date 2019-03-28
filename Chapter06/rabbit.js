51cadb72446f54ee391e2d071b141d3a45571afe
ago <ago@sitecore.net>
2019-03-28 17:42:57 +0200

    rabbit.js added

---
+++ Chapter06/rabbit.js
@@ -0,0 +1,32 @@
+let rabbit = {
+    type: "common",
+    speak: speak
+};
+
+rabbit.speak("I'm alive");
+
+function speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+}
+
+let whiteRabbit = {
+    type: "white",
+    speak
+};
+let hungryRabbit = {
+    type: "hungry",
+    speak
+};
+
+whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!");
+hungryRabbit.speak("I could use a carrot right now.");
+
+
+function normalize() {
+    console.log(this.coords.map(n => n / this.length));
+}
+
+normalize.call({
+    coords: [0, 2, 3],
+    length: 5
+});
