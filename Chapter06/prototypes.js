fc3dd8d459ba511c2d86427aff3edcdca08b9c36
ago <ago@sitecore.net>
2019-03-29 16:05:40 +0200

    prototypes and classes notation samples

--- Chapter06/prototypes.js
+++ Chapter06/prototypes.js
@@ -15,3 +15,37 @@ let protoRabbit = {
 let killerRabbit = Object.create(protoRabbit);
 killerRabbit.type = "killer";
 killerRabbit.speak("SKREEE!");
+
+
+// constructor
+function makeRabbit(type) {
+    let rabbit = Object.create(protoRabbit);
+    rabbit.type = type;
+    return rabbit;
+}
+
+function Rabbit(type) {
+    this.type = type;
+}
+
+Rabbit.prototype.speak = function (line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+}
+
+let weirdRabbit = new Rabbit("weird");
+weirdRabbit.speak("Hi!");
+
+class RabbitClass {
+    constructor(type) {
+        this.type = type;
+    }
+    speak(line) {
+        console.log(`The ${this.type} rabbit says '${line}'`);
+    }
+}
+
+let killerRabbit2 = new RabbitClass("killer");
+let blackRabbit = new RabbitClass("black");
+
+killerRabbit.speak("Hi, I'm a killer");
+blackRabbit.speak("Hi, I'm black");
