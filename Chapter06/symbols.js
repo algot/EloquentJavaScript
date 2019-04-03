9e2f340d47258c75d0a290a610317e2c69fc2ec2
ago <ago@sitecore.net>
2019-04-03 14:57:11 +0300

    Sympols added

---
+++ Chapter06/symbols.js
@@ -0,0 +1,12 @@
+const toStringSymbol = Symbol("toString");
+Array.prototype[toStringSymbol] = function(){
+    return `${this.length} cm of blue yarn`;
+};
+
+console.log([1, 2].toString());
+console.log([1, 2][toStringSymbol]());
+
+let stringObject = {
+    [toStringSymbol]() { return "a jute rope"; }
+};
+console.log(stringObject[toStringSymbol]());
