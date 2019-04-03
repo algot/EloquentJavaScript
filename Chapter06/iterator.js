8fa34cfe2a41cf4f721ce0e4841e39ae177443b4
ago <ago@sitecore.net>
2019-04-03 18:51:54 +0300

    iterator finished

--- Chapter06/iterator.js
+++ Chapter06/iterator.js
@@ -24,4 +24,38 @@ class Matrix {
     }
 }
 
-var matrix = new Matrix(2, 2);
+Matrix.prototype[Symbol.iterator] = function () {
+    return new MatrixIterator(this);
+};
+
+class MatrixIterator {
+    constructor(matrix) {
+        this.x = 0;
+        this.y = 0;
+        this.matrix = matrix;
+    }
+
+    next() {
+        if (this.y == this.matrix.height) return {
+            done: true
+        };
+        let value = {
+            x: this.x,
+            y: this.y,
+            value: this.matrix.get(this.x, this.y)
+        };
+        this.x++;
+        if (this.x == this.matrix.width) {
+            this.x = 0;
+            this.y++;
+        }
+        return {
+            value,
+            done: false
+        };
+    }
+}
+
+var matrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
+var matrixIterator = new MatrixIterator(matrix);
+var result = matrixIterator.next();
