fbc8fa5e7c7eec41d272e7fc8e85f56e9e5718a5
ago <ago@sitecore.net>
2019-04-08 13:37:04 +0300

    borrowingAMethod added

---
+++ Chapter06/borrowingAMethod.js
@@ -0,0 +1,8 @@
+let map = {
+    one: true,
+    two: true,
+    hasOwnProperty: true
+};
+
+// console.log(map.hasOwnProperty("one"));
+console.log(Object.prototype.hasOwnProperty.call(map, "one"));
