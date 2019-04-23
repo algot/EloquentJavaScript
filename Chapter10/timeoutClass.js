719c6618b9ad501329307766e0b0b697e9ee2748
ago <ago@sitecore.net>
2019-04-23 17:04:02 +0300

    Chapte10 finished

---
+++ Chapter10/timeoutClass.js
@@ -0,0 +1,45 @@
+class Timeout extends Error {}
+
+function request(nest, target, type, content) {
+    return new Promise((resolve, reject) => {
+        let done = false;
+
+        function attempt(n) {
+            nest.send(target, type, content, (failed, value) => {
+                done = true;
+                if (failed) reject(failed);
+                else resolve(value);
+            });
+            setTimeout(() => {
+                if (done) return;
+                else if (n < 3) attempt(n + 1);
+                else reject(new Timeout("Timed out"));
+            }, 250);
+        }
+        attempt(1);
+    });
+}
+
+function requestType(name, handler) {
+    defineRequestType(name, (nest, content, source, callback) => {
+        try {
+            Promise.resolve(handler(nest, content, source))
+                .then(response => callback(null, response),
+                    failure => callback(failure));
+        } catch (exception) {
+            callback(exception);
+        }
+    });
+}
+
+requestType("ping", () => "pong");
+
+function availableNeighbors(nest) {
+    let request = nest.neighbors.map(neighbor => {
+        return request(nest, neighbor, "ping")
+            .then(() => true, () => false);
+    });
+    return Promise.all(requests).then(result => {
+        return nest.neighbors.filter((_, i) => result[i]);
+    });
+}
